695d17008ea89b0e4c4b317a05891ed047fae1ea
tcole <tcole@DESKTOP-ET2ANPG>
2018-11-26 15:05:56 -0500

    Set up trail object, basic API access in controller

--- src/java/edu/vt/EntityBeans/Trail.java
+++ src/java/edu/vt/EntityBeans/Trail.java
@@ -4,14 +4,199 @@
  */
 package edu.vt.EntityBeans;
 
+import java.util.Date;
+
 public class Trail {
   // Properties
   private Long id;
   private String name;
-  // TODO
+  private String location;
+  private String url;
+  private String summary;
+  private String difficulty;
+  private Double rating;
+  private String imgUrl;
+  
+  private Double longitude;
+  private Double latitude;
+  
+  private Double length;
+  private Double ascentDist;
+  private Double descentDist;
+  private Double highestHeight;
+  private Double lowestHeight;
+  
+  private String conditionStatus;
+  private String conditionDetails;
+  private Date conditionDate;
   
   // Constructor
   public Trail() {
   }
   
+  public Trail(Long id, String name, String location, String url, String summary, String difficulty, Double rating, String imgUrl, Double longitude, Double latitude, Double length, Double ascentDist, Double descentDist, Double highestHeight, Double lowestHeight, String conditionStatus, String conditionDetails, Date conditionDate) {
+    this.id = id;
+    this.name = name;
+    this.location = location;
+    this.url = url;
+    this.summary = summary;
+    this.difficulty = difficulty;
+    this.rating = rating;
+    this.imgUrl = imgUrl;
+    this.longitude = longitude;
+    this.latitude = latitude;
+    this.length = length;
+    this.ascentDist = ascentDist;
+    this.descentDist = descentDist;
+    this.highestHeight = highestHeight;
+    this.lowestHeight = lowestHeight;
+    this.conditionStatus = conditionStatus;
+    this.conditionDetails = conditionDetails;
+    this.conditionDate = conditionDate;
+  }
+  
+  // Getters/setters
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getSummary() {
+    return summary;
+  }
+
+  public void setSummary(String summary) {
+    this.summary = summary;
+  }
+
+  public String getDifficulty() {
+    return difficulty;
+  }
+
+  public void setDifficulty(String difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  public Double getRating() {
+    return rating;
+  }
+
+  public void setRating(Double rating) {
+    this.rating = rating;
+  }
+
+  public String getImgUrl() {
+    return imgUrl;
+  }
+
+  public void setImgUrl(String imgUrl) {
+    this.imgUrl = imgUrl;
+  }
+
+  public Double getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(Double longitude) {
+    this.longitude = longitude;
+  }
+
+  public Double getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+  }
+
+  public Double getLength() {
+    return length;
+  }
+
+  public void setLength(Double length) {
+    this.length = length;
+  }
+
+  public Double getAscentDist() {
+    return ascentDist;
+  }
+
+  public void setAscentDist(Double ascentDist) {
+    this.ascentDist = ascentDist;
+  }
+
+  public Double getDescentDist() {
+    return descentDist;
+  }
+
+  public void setDescentDist(Double descentDist) {
+    this.descentDist = descentDist;
+  }
+
+  public Double getHighestHeight() {
+    return highestHeight;
+  }
+
+  public void setHighestHeight(Double highestHeight) {
+    this.highestHeight = highestHeight;
+  }
+
+  public Double getLowestHeight() {
+    return lowestHeight;
+  }
+
+  public void setLowestHeight(Double lowestHeight) {
+    this.lowestHeight = lowestHeight;
+  }
+
+  public String getConditionStatus() {
+    return conditionStatus;
+  }
+
+  public void setConditionStatus(String conditionStatus) {
+    this.conditionStatus = conditionStatus;
+  }
+
+  public String getConditionDetails() {
+    return conditionDetails;
+  }
+
+  public void setConditionDetails(String conditionDetails) {
+    this.conditionDetails = conditionDetails;
+  }
+
+  public Date getConditionDate() {
+    return conditionDate;
+  }
+
+  public void setConditionDate(Date conditionDate) {
+    this.conditionDate = conditionDate;
+  }
 }
--- src/java/edu/vt/controllers/TrailController.java
+++ src/java/edu/vt/controllers/TrailController.java
@@ -4,19 +4,180 @@
  */
 package edu.vt.controllers;
 
+import edu.vt.EntityBeans.Trail;
+import edu.vt.globals.Methods;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import javax.inject.Named;
-import javax.enterprise.context.Dependent;
+import javax.enterprise.context.SessionScoped;
+import org.primefaces.json.JSONArray;
+import org.primefaces.json.JSONObject;
 
+@SessionScoped
 @Named(value = "trailController")
-@Dependent
-public class TrailController {
-  // TODO
+public class TrailController implements Serializable {
+
   // API information
-  private final String apiUrl
-    = "";
+  private final String apiKey = "&key=200392046-9c791c4bdcbda3ac77e4fea595465cf7";
+  private final String apiUrl = "https://www.hikingproject.com/data/";
+
+  // Properties
+  private List<Trail> trailList;
+  private Trail selected;
 
   // Constructor
   public TrailController() {
   }
+
+  // Get a trail's information via its ID.
+  // TODO: testing
+  public Trail getTrailByID(int ID) {
+    try {
+      String jsonStr = readUrlContent(apiUrl + "get-trails-by-id?ids=" + ID + apiKey);
+      JSONObject jsonData = (JSONObject) new JSONObject(jsonStr);
+      if (jsonData.optInt("success", 0) == 0) {
+        Methods.showMessage("Fatal Error", "No trail found with ID " + ID + ".", "");
+      } else {
+        JSONObject trail = (JSONObject) jsonData.getJSONArray("trails").get(0);
+        return createTrailFromJSON(trail);
+      }
+    } catch (Exception ex) {
+      Methods.showMessage("Fatal Error",
+              "Hiking project API is unreachable!",
+              "See: " + ex.getMessage());
+    }
+    return null;
+  }
   
+  // Get trails within an area
+  // TODO: testing
+  public List<Trail> getTrailsInRadius(double lat, double lon, double maxDistance) {
+    String searchData = String.format("?lat=%f&lon=&f&maxDistance=&f", lat, lon, maxDistance);
+    try {
+      String jsonStr = readUrlContent(apiUrl + "getTrails" + searchData + apiKey);
+      JSONObject jsonData = (JSONObject) new JSONObject(jsonStr);
+      if (jsonData.optInt("success", 0) == 0) {
+        Methods.showMessage("Fatal Error", "No trails found within range.", "");
+      } else {
+        List<Trail> trails = new ArrayList<Trail>();
+        JSONArray trailArray = jsonData.getJSONArray("trails");
+        trailArray.forEach(trailObj -> {
+          trails.add(createTrailFromJSON((JSONObject) trailObj));
+        });
+        return trails;
+      }
+    } catch (Exception ex) {
+      Methods.showMessage("Fatal Error",
+              "Hiking project API is unreachable!",
+              "See: " + ex.getMessage());
+    }
+    return null;
+  }
+
+  /**
+   * Create a trail object from a given JSON object.
+   *
+   * @param trailJson The JSON to parse.
+   * @return The created trail.
+   */
+  private Trail createTrailFromJSON(JSONObject trailJson) {
+    Long id = trailJson.optLong("id", 0);
+    if (id == 0) {
+      return null;
+    }
+
+    Double longitude = trailJson.optDouble("longitude");
+    if (longitude == Double.NaN) {
+      return null;
+    }
+    Double latitude = trailJson.optDouble("latitude");
+    if (latitude == Double.NaN) {
+      return null;
+    }
+    
+    String name = trailJson.optString("name", "");
+    if (name.equals("")) {
+      name = "Name unavailable.";
+    }
+    String location = trailJson.optString("location", "");
+    if (location.equals("")) {
+      location = "Location name unavailable.";
+    }
+    String url = trailJson.optString("url", "");
+    String summary = trailJson.optString("summary", "");
+    if (summary.equals("")) {
+      summary = "No summary available.";
+    }
+    String difficulty = trailJson.optString("difficulty", "");
+    if (difficulty.equals("")) {
+      difficulty = "Difficulty unavailable.";
+    }
+    Double rating = trailJson.optDouble("stars", 0);
+    String imgUrl = trailJson.optString("imgMedium", ""); // TODO: default image?
+
+    Double length = trailJson.optDouble("length");
+    Double ascentDist = trailJson.optDouble("ascent");
+    Double descentDist = trailJson.optDouble("descent");
+    Double highestHeight = trailJson.optDouble("high");
+    Double lowestHeight = trailJson.optDouble("low");
+    
+    String conditionStatus = trailJson.optString("conditionStatus", "");
+    if (conditionStatus.equals("")) {
+      conditionStatus = "Condition status unavailable.";
+    }
+    String conditionDetails = trailJson.optString("conditionDetails", "");
+    if (conditionDetails.equals("")) {
+      conditionDetails = "Condition details unavailable.";
+    }
+    Date conditionDate;
+    String conditionDateStr = trailJson.optString("conditionDate", "");
+    try {
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      conditionDate = dateFormat.parse(conditionDateStr);
+    } catch (ParseException ex) {
+      conditionDate = null;
+    }
+    
+    return new Trail(
+            id, name, location, url, summary, difficulty,
+            rating, imgUrl, longitude, latitude, length,
+            ascentDist, descentDist, highestHeight, lowestHeight,
+            conditionStatus, conditionDetails, conditionDate
+    );
+  }
+
+  /**
+   * Return the content of a given URL as String
+   *
+   * @param webServiceURL to retrieve the JSON data from
+   * @return JSON data from the given URL as String
+   * @throws Exception
+   */
+  public String readUrlContent(String webServiceURL) throws Exception {
+    BufferedReader reader = null;
+    try {
+      URL url = new URL(webServiceURL);
+      reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      StringBuilder buffer = new StringBuilder();
+      char[] chars = new char[10240];
+
+      int numberOfCharactersRead;
+      while ((numberOfCharactersRead = reader.read(chars)) != -1) {
+        buffer.append(chars, 0, numberOfCharactersRead);
+      }
+      return buffer.toString();
+
+    } finally {
+      if (reader != null) {
+        reader.close();
+      }
+    }
+  }
 }
