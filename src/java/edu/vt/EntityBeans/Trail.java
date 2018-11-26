2f8335c5422653182c11ae5bfa0f72ecb1fb6930
tcole <tcole@DESKTOP-ET2ANPG>
2018-11-26 00:40:32 -0500

    Couple of base files for handling trail info, TODO

---
+++ src/java/edu/vt/EntityBeans/Trail.java
@@ -0,0 +1,17 @@
+/*
+ * Created by Thomas Coleman on 2018.11.15
+ * Copyright © 2018 Thomas Coleman. All rights reserved.
+ */
+package edu.vt.EntityBeans;
+
+public class Trail {
+  // Properties
+  private Long id;
+  private String name;
+  // TODO
+  
+  // Constructor
+  public Trail() {
+  }
+  
+}
---
+++ src/java/edu/vt/controllers/TrailController.java
@@ -0,0 +1,22 @@
+/*
+ * Created by Thomas Coleman on 2018.11.15
+ * Copyright © 2018 Thomas Coleman. All rights reserved.
+ */
+package edu.vt.controllers;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+@Named(value = "trailController")
+@Dependent
+public class TrailController {
+  // TODO
+  // API information
+  private final String apiUrl
+    = "";
+
+  // Constructor
+  public TrailController() {
+  }
+  
+}
